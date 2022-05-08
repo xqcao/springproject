845693557c85c0e0f77013647f604ada09f55dd6
xqcao <xqcaoumail@gmail.com>
2022-05-08 18:25:25 -0400

    1st project init

---
+++ departmentservice/src/test/java/com/departmentservice/departmentservice/DepartmentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.departmentservice.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
